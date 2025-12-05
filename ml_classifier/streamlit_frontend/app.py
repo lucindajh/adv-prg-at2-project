ed02c6ed715eaa4686ec750db6b2927ad9e2d020
20110825 <20110825@tafe.wa.edu.au>
2025-12-05 16:12:00 +0800

    feat: added code to display image, commented out rest api call (keeping for future reference)

--- ml_classifier/streamlit_frontend/app.py
+++ ml_classifier/streamlit_frontend/app.py
@@ -13,30 +13,36 @@ streamlit.write("Upload a jpeg file to classify it into one of the imagenet cate
 # --- Input Form ---
 uploaded_file = streamlit.file_uploader("Upload a .jpg file", type=["jpg"])
 
+if uploaded_file:
+    image = Image.open(uploaded_file)
+    streamlit.image(image, caption='Uploaded image.', use_column_width=True)
+
 if streamlit.button("Predict"):
 
     if not uploaded_file:
         streamlit.warning("Please provide text or upload a .jpg file.")
     else:
-        image = uploaded_file.getvalue()
-        try:
+        image = Image.open(uploaded_file)
 
-            files = {
-                "file": (
-                    uploaded_file.name or "upload.txt",
-                    image,
-                    "text/plain",
-                )
-            }
-            response = requests.post(
-                "http://localhost:8000/api/predict/",
-                files=files,
-            )
 
-            if response.status_code == 200:
-                prediction = response.json().get("prediction")
-                streamlit.success(f"Prediction: {prediction}")
-            else:
-                streamlit.error(f"API error {response.status_code}: {response.text}")
-        except requests.exceptions.ConnectionError:
-              streamlit.error("Could not connect to Django backend. Is the server running?")
+        # try:
+        #
+        #     files = {
+        #         "file": (
+        #             uploaded_file.name or "upload.txt",
+        #             image,
+        #             "text/plain",
+        #         )
+        #     }
+        #     response = requests.post(
+        #         "http://localhost:8000/api/predict/",
+        #         files=files,
+        #     )
+        #
+        #     if response.status_code == 200:
+        #         prediction = response.json().get("prediction")
+        #         streamlit.success(f"Prediction: {prediction}")
+        #     else:
+        #         streamlit.error(f"API error {response.status_code}: {response.text}")
+        # except requests.exceptions.ConnectionError:
+        #       streamlit.error("Could not connect to Django backend. Is the server running?")
