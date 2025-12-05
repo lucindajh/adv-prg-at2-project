6fc0e9e02c74dc6e410e4b99cc2d49cf9af7b103
20110825 <20110825@tafe.wa.edu.au>
2025-12-05 18:31:13 +0800

    feat: added Prediction model with fields for time of prediction, user, class name and probability

--- ml_classifier/classifier/models.py
+++ ml_classifier/classifier/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 # Create your models here.
-
+class Prediction(model.Model):
+    """Represents prediction metadata from prediction process"""
+    predicted_at = models.DateTimeField(auto_now_add=True)
+    user = models.CharField(max_length=30)
+    imagenet_class = models.TextField(max_length=30)
+    probability = models.DecimalField()
