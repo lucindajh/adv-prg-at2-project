754e49dccd9f45a567be39caff7b7dcb25c0f4a1
20110825 <20110825@tafe.wa.edu.au>
2025-12-06 14:05:22 +0800

    fix: changed 'model.Model' to 'models.Model' in Prediction class

--- ml_classifier/classifier/models.py
+++ ml_classifier/classifier/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class Prediction(model.Model):
+class Prediction(models.Model):
     """Represents prediction metadata from prediction process"""
     predicted_at = models.DateTimeField(auto_now_add=True)
     user = models.CharField(max_length=30)
