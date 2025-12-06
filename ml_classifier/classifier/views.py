df6a010eb024382bbefb5c03601cacd20f4b72c2
20110825 <20110825@tafe.wa.edu.au>
2025-12-06 14:11:47 +0800

    fix: imported csrf_exempt decorator in classifier views

--- ml_classifier/classifier/views.py
+++ ml_classifier/classifier/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from ml.predict import predict
 from classifier.models import Prediction
 from PIL import Image
