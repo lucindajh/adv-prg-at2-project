e7e94872d8db435ea3835a787dbfbd1d3ace703c
20110825 <20110825@tafe.wa.edu.au>
2025-12-08 13:27:21 +0800

    chore: added docstrings to services

--- ml_classifier/classifier/services.py
+++ ml_classifier/classifier/services.py
@@ -9,6 +9,7 @@ logger.addHandler(file_handler)
 
 
 def action_logger(func):
+    """ wrapper to log function calls """
     def wrapper(*args, **kwargs):
         user = args[0]
         current_datetime = datetime.now()
@@ -18,12 +19,24 @@ def action_logger(func):
 
 
 def read_logs(log_file_path: str):
+    """ reads logs line by line from the given log file
+    Args:
+        log_file_path (str): the file path to read from
+    Returns:
+        Generator[str]: the file yield
+    """
     with open(log_file_path, "r") as file:
         for line in file:
             yield line.strip()
 
 
 def serialise_prediction(prediction: Prediction) -> dict:
+    """ takes a Prediction object and serialises it to a dictionary
+    Args:
+        prediction (Prediction): the prediction object to serialise
+    Returns:
+        dict: the dictionary representing the original object
+    """
     return {
         "predicted_at": prediction.predicted_at,
         "user": prediction.user,
